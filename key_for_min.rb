bd5d33f4e9d6accdfb51da00efb5c0dde691c4fa
Yancy Zhang <yuanxi.zhang@gmail.com>
2018-06-06 19:50:07 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,8 +2,8 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  name_hash.sort_by { |name, age| age }.first
-    
+  name, age = name_hash.sort_by { |name, age| age }.first
+  name 
   
 
 end
